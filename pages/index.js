20d18806c33e133222861b4ad5e4ad8bc4542cb2
GilbertoBJ <allioth.gj@gmail.com>
2024-10-14 23:10:52 +0000

    Atalização Da frase0.1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home (){
-    return <h1>Oi Minhas vidas. Amo você , Pra mostrar seu amor por mim imita um suricato!! :)</h1>
+    return <h1>Oi Minhas vidas. Amo você , Pra mostrar seu amor por mim imita um suricato!!🤣😍🐹 :)</h1>
 }
 
 export default home
